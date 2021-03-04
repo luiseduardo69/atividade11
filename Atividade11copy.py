63a6f9f6a5a302d818df03318a20e9a628e1304c
luiseduardo69 <72415551+luiseduardo69@users.noreply.github.com>
2021-03-03 21:12:08 -0300

    Update Atividade11copy.py

--- Atividade11copy.py
+++ Atividade11copy.py
@@ -3,11 +3,8 @@ from Atividade03 import Quadrilátero
 class Play:
     def P(self):
         g = Ponto(5,5)
-        #definir ponto no plano
         print(g.qualQuadrante())
         f = Quadrilátero(10,7)
-        #definir tamanho do quadrilátero
         print(f.contidoEmQ(g))
-
 a = Play()
 a.P()
